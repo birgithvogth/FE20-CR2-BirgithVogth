158b28fee1b6f687e0106b1afcbae7cd5c37f9d9
birgithvogth <birgithvogth1@icloud.com>
2023-10-06 12:51:54 +0200

    first push just the basics images bootstrap etc

---
+++ data.js
@@ -0,0 +1,91 @@
+let tasks = `[{ 
+
+     taskName: "vet", 
+    
+     image: "Images/baghira.jpeg", 
+    
+     description: "Go to the vet with Baghira", 
+    
+     importance: 0    
+    
+},{ 
+
+     taskName: "shopping", 
+        
+     image: "Images/shopping.jpg", 
+        
+     description: "Go shopping, enjoy yourself.", 
+        
+     importance: 0    
+        
+},{ 
+
+     taskName: "dry-cleaning", 
+            
+     image: "Images/coat.jpg", 
+            
+     description: "Take fur coat to the dry-cleaning.", 
+            
+     importance: 0    
+            
+},{ 
+
+     taskName: "groceries", 
+                
+     image: "Images/groceries.jpg", 
+                
+     description: "Shop for weekly groceries at Billa and Hofer.", 
+                
+     importance: 0    
+                
+},{ 
+
+     taskName: "Cat Food", 
+                    
+     image: "Images/catfood.jpg", 
+                    
+     description: "Pick up cat food from the P.O. Box.", 
+                    
+     importance: 0    
+                    
+},{ 
+
+     taskName: "groom Merlin", 
+                        
+     image: "Images/merlin.jpeg", 
+                        
+     description: "Groom Merlins fur.", 
+                        
+     importance: 0    
+                        
+},{ 
+
+     taskName: "groom Pearl", 
+                            
+     image: "Images/pearl.jpeg", 
+                            
+     description: "Groom Pearls fur.", 
+                            
+     importance: 0    
+                            
+},{ 
+
+     taskName: "nail clipping", 
+                                
+     image: "Images/felia.jpeg", 
+                                
+     description: "Shorten Felias claws.", 
+                                
+     importance: 0    
+                                
+},{ 
+
+     taskName: "learning", 
+                                    
+     image: "Images/learning.jpg", 
+                                    
+     description: "Learn coding for Code-factory Full-Stack Course.", 
+     
+     importance: 0    
+                                    
+;}]`
