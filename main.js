0ad524d077e2d7ef94d8952619ff840a09e3c5ae
birgithvogth <birgithvogth1@icloud.com>
2023-12-17 01:40:14 +0100

    modified

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ console.log(newTasks)
 newTasks.forEach(task => {
     document.getElementById("result").innerHTML += `
     <div class="card">
-  <img src="${task.image}" class="card-img-top" alt="${task.taskName}">
-  <div class="card-body">
-    <h5 class="card-title">${task.taskName}</h5>
-    <p class="card-text">${task.description}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16">
+
+    <div class="container-card bg-green-box">
+<img src="${task.image}" class="card-img-top" alt="${task.taskName}">
+    <p class="card-title">${task.taskName}</p>
+    <p class="card-description">${task.description}</p>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16" id="bookmark">
   <path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
 </svg>
-    <span>Priority:</span>
+    <span id="priority">Priority:</span>
     <button class="btn btn-success myButton" id="changeColor"> <span>${task.importance}</span></button>
   </div>
 </div>
@@ -29,7 +31,7 @@ newTasks.forEach(task => {
             buttons[i].classList.remove( "btn-success");
             buttons[i].classList.add("btn-warning");
         } else if (newTasks[i].importance >= 4 && newTasks[i].importance <= 5) {
-            buttons[i].classList.remove("btn-success", "btn-warning");
+            buttons[i].classList.remove("btn-primary", "btn-success", "btn-warning");
             buttons[i].classList.add("btn-danger");
         }
     }
